eba7481c056fef9c6be2589404a0031c037f943b
Arnaud Dessein <adessein@protonmail.com>
2024-03-14 21:52:19 +0100

    Continue to fix

--- command.sh
+++ command.sh
@@ -1,1 +1,5 @@
-ebook-convert index.html Laeremateriale_til_Indfodsretsproven.epub --level1-toc=//h:h1 --level2-toc=//h:h2 --level3-toc=//h:h3
+ebook-convert index.html Laeremateriale_til_Indfodsretsproven.azw3 --level1-toc=//h:h1 --level2-toc=//h:h2 --level3-toc=//h:h3
+
+perl -p -i -e 's/change this/to that/g' index.html
+perl -p -i -e 's/(\<h[1-4]\>)([^\<]+)(\<\/h[1-4]>)/$1\L$2$3/gm' index.html
+
